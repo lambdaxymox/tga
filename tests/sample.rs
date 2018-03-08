7c005889572df733ff15cd61dca02c408e143f0e
LambdaXymox <lambda.xymox@gmail.com>
2018-03-08 21:17:46 +0000

    Add RLE encoded images to test data.

--- tests/sample.rs
+++ tests/sample.rs
@@ -19,27 +19,46 @@ pub const ONE_TGA_BITS_PER_PIXEL: usize = 24;
 pub const ONE_TGA_COLOR_MAP_TYPE: usize = 0;
 pub const ONE_TGA_DATA_TYPE_CODE: usize = 2;
 
+pub const LENA_RLE_TGA: &str = "sample/lena_rle.tga";
+pub const LENA_RLE_TGA_WIDTH: usize = 512;
+pub const LENA_RLE_TGA_HEIGHT: usize = 512;
+pub const LENA_RLE_TGA_BITS_PER_PIXEL: usize = 24;
+pub const LENA_RLE_TGA_COLOR_MAP_TYPE: usize = 0;
+pub const LENA_RLE_TGA_DATA_TYPE_CODE: usize = 2;
+
+pub const COLOR_RLE_TGA: &str = "sample/color_rle.tga";
+pub const COLOR_RLE_TGA_WIDTH: usize = 640;
+pub const COLOR_RLE_TGA_HEIGHT: usize = 480;
+pub const COLOR_RLE_TGA_BITS_PER_PIXEL: usize = 24;
+pub const COLOR_RLE_TGA_COLOR_MAP_TYPE: usize = 0;
+pub const COLOR_RLE_TGA_DATA_TYPE_CODE: usize = 2;
+
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
+
+    struct TestCase<'a> {
+        data: Vec<&'a str>,
+    }
+
+    fn test_cases<'a>() -> TestCase<'a> {
+        TestCase {
+            data: vec![
+                super::LENA_TGA,     super::COLOR_TGA,     super::ONE_TGA, 
+                super::LENA_RLE_TGA, super::COLOR_RLE_TGA,
+            ]
+        }
+    }
 
     fn file_exists(file: &str) -> bool {
         File::open(file).is_ok()
     }
 
     #[test]
-    fn test_file_exists1() {
-        assert!(file_exists(super::LENA_TGA));
-    }
-
-    #[test]
-    fn test_file_exists2() {
-        assert!(file_exists(super::COLOR_TGA));
-    }
-
-    #[test]
-    fn test_file_exists3() {
-        assert!(file_exists(super::ONE_TGA));
+    fn test_files_exist() {
+        for test_case in test_cases().data {
+            assert!(file_exists(test_case));
+        }
     }
 }

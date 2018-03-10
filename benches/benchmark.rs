b2fa164a56a31cfa1fc6056a5d91fbb702bf5268
LambdaXymox <lambda.xymox@gmail.com>
2018-03-10 18:42:43 +0000

    Add benchmark for RLE compression.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -42,5 +42,32 @@ fn benchmark(c: &mut Criterion) {
     c.bench_functions("TGA Parser", functions, sample::LENA_TGA);
 }
 
-criterion_group!(name = benches; config = config(); targets = benchmark);
+fn benchmark_rle(c: &mut Criterion) {
+    let control = Fun::new("Control", |b, filename| b.iter(|| {
+        let mut file = File::open(filename).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+
+        buffer
+    }));
+    
+    let parse_from_file = Fun::new("ParseFromFile", |b, filename| b.iter(|| {
+        let mut file = File::open(filename).unwrap();
+        TgaImage::parse_from_file(&mut file)
+    }));
+
+    let parse_from_buffer = Fun::new("ParseFromBuffer", |b, filename| b.iter(|| { 
+        let mut file = File::open(filename).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+
+        TgaImage::parse_from_buffer(&buffer)
+    }));
+
+    let functions = vec![control, parse_from_file, parse_from_buffer];
+
+    c.bench_functions("TGA Parser RLE", functions, sample::LENA_RLE_TGA);
+}
+
+criterion_group!(name = benches; config = config(); targets = benchmark, benchmark_rle);
 criterion_main!(benches);
--- benches/sample.rs
+++ benches/sample.rs
@@ -1,1 +1,2 @@
 pub const LENA_TGA: &str = "sample/lena.tga";
+pub const LENA_RLE_TGA: &str = "sample/lena_rle.tga";
